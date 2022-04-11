600eb009fca7ba95ca82c51bcbf8847c7840667b
corbincargil <corbin.cargil@gmail.com>
2022-04-11 17:29:54 -0500

    Added a 20x20 grid of square divs into the 'main-container'.

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//declaring some variables:
+let cellNumber = 400; //Number of cells for the grid. In this case it is 16x16.
+let cellNumberArray = [];
 mainContainer = document.getElementById('main-container');
 
-const cell = document.createElement('div');
+//const cell = document.createElement('div');
+
+//mainContainer.appendChild(document.createElement('div'));
 
 
-//mainContainer.appendChild(gridBox);
+
+//creates an array of consecutive numbers starting at 1, ending at cellNumber
+for (let i = 0; i < cellNumber; i++) {
+    cellNumberArray.push(i+1);
+}
+
+cellNumberArray.forEach( () => {
+    cell = document.createElement('div');
+    mainContainer.appendChild(cell);
+    });
